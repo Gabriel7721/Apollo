4519649fc1be7d7d0ae1433b69847dc6a259c490
ThanhLe9624 <114575764+ThanhLe9624@users.noreply.github.com>
2024-06-01 01:01:27 +0700

    Update OrderDeliveryRepository.java

--- Library/src/main/java/com/ecommerce/library/repository/OrderDeliveryRepository.java
+++ Library/src/main/java/com/ecommerce/library/repository/OrderDeliveryRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderDeliveryRepository extends JpaRepository<OrderDelivery, Integer> {
-    OrderDelivery findOrderDeliveriesBy(Long id);
+public interface OrderDeliveryRepository extends JpaRepository<OrderDelivery, Long> {
+    OrderDelivery findOrderDeliveryById(Long id);
 }
